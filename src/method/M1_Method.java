da0f71c71cf63da8b261feb48a1f11634fe5e8d9
vedashree <vedashreepatil07@gmail.com>
2023-10-28 21:51:29 +0530

    initial Commit.

---
+++ src/method/M1_Method.java
@@ -0,0 +1,15 @@
+package method;
+class Animal{   //craete class
+	public void eat() {  // create methhod
+		System.out.println("I am eating");
+	}
+}
+public class M1_Method {
+
+	public static void main(String[] args) {
+		Animal tommy =new Animal();  //create object  -->tommy is object
+		tommy.eat(); //call method
+
+	}
+
+}
